368d24b3bcc776f96a5dc75498f52b34b67f5244
betty2310 <huynh.dh2310@gmail.com>
2023-01-01 10:05:53 +0700

    update prob 118

---
+++ array/118.pascals-triangle.cpp
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode id=118 lang=cpp
+ *
+ * [118] Pascal's Triangle
+ */
+
+// @lc code=start
+#include <algorithm>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+   public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> ans;
+        for (int i = 0; i < numRows; ++i) {
+            vector<int> row(i + 1, 1);
+            for (int j = 1; j < i; j++) {
+                row[j] = ans[i - 1][j] + ans[i - 1][j - 1];
+            }
+            ans.push_back(row);
+        }
+        return ans;
+    }
+};
+// @lc code=end
